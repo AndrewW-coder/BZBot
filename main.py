58f938236d33242a0d237bca71c6d9729340f524
Andrew Wang <Its.Andrew.Wang.2007@gmail.com>
2025-07-30 10:49:27 -0700

    Added some hypixel bz features/api work

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import logging
 from dotenv import load_dotenv
 import os
 
+import requests
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
@@ -15,6 +17,13 @@ intents.presences = True
 
 bot = commands.Bot(command_prefix='!', intents = intents)
 
+def getID(name):
+    url = f"https://api.mojang.com/users/profiles/minecraft/{name}"
+    response = requests.get(url)
+    data = response.json()
+
+    return data["id"]
+
 @bot.event
 async def on_ready():
     print("Ready")
@@ -23,6 +32,34 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
+    
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def profile(ctx, *, msg):
+    await ctx.send(f"ID: {getID(msg)}")
+
+@bot.command()
+async def bz(ctx, *, msg):
+    url = "https://api.hypixel.net/v2/skyblock/bazaar"
+    response = requests.get(url)
+    if(response.status_code == 200):
+        item = msg.upper()
+        item = item.replace(' ', '_')
+        data = response.json()
+
+        ii = data["products"][item]
+        
+        embed = discord.Embed(title = f"{item}", description = f"Insta-Buy: {round(ii["quick_status"]["buyPrice"], 2)}\nSell Order: {round(ii["quick_status"]["sellPrice"], 2)}")
+        await ctx.send(embed = embed)
+    else:
+        await ctx.send("Error: Invalid Input")
+
     
 
 bot.run(token, log_handler = handler, log_level = logging.DEBUG)
